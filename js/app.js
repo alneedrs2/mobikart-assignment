1a35bdafb09bf20ade760254c399081ed0637320
Alneed Roy <alneedrs2@gmail.com>
2022-03-01 00:08:09 +0600

    successful card api part done

--- js/app.js
+++ js/app.js
@@ -5,9 +5,6 @@ const searchPhone = () => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayPhones(data.data));
-  // .catch((error) =>
-  //   displayError("Something Went Wrong!! Please try again later!")
-  // );
 };
 
 const displayPhones = (phones) => {
@@ -15,15 +12,14 @@ const displayPhones = (phones) => {
   phoneContainer.innerHTML = "";
   phones.forEach((phone) => {
     const phoneDiv = document.createElement("div");
-    phoneDiv.className = "single-result row align-items-center my-3 p-3";
+    phoneDiv.className = "single-result col-lg-4 my-3 p-3";
     phoneDiv.innerHTML = `
       <div class="phone-details">
       <div class="profile-pic w-50 m-auto rounded">
-          <img class="w-50" src="" alt="">
+          <img class="w-50" src="${phone.image}" alt="">
       </div>
-      <h1>Phone Name: cool</h1>
-      <h3>Brand:oh that working</h3>
-      <h4>Slug: do</h4>
+      <h3>Phone Name: ${phone.phone_name}</h3>
+      <h4>Brand: ${phone.brand} </h4>
   </div>
   `;
     phoneContainer.appendChild(phoneDiv);
