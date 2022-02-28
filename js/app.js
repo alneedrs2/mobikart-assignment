409fcfd1c8695bc0f89099dcb748ca63f11627ed
Alneed Roy <alneedrs2@gmail.com>
2022-02-28 23:36:31 +0600

    card added for api

--- js/app.js
+++ js/app.js
@@ -4,28 +4,28 @@ const searchPhone = () => {
   // load data
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayPhones(data.data));
   // .catch((error) =>
   //   displayError("Something Went Wrong!! Please try again later!")
   // );
 };
 
-// const displayPhones = (phones) => {
-//   const phoneContainer = document.getElementById("phone-container");
-//   phoneContainer.innerHTML = "";
-//   phones.forEach((data) => {
-//     const phoneDiv = document.createElement("div");
-//     phoneDiv.className = "single-result row align-items-center my-3 p-3";
-//     phoneDiv.innerHTML = `
-//     <div class="phone-details">
-//     <div class="profile-pic w-50 m-auto rounded">
-//         <img class="w-50" src="${data.image}" alt="">
-//     </div>
-//     <h1>Phone Name: ${data.phone_name}</h1>
-//     <h3>Brand:${data.brand} </h3>
-//     <h4>Slug: ${data.slug}</h4>
-// </div>
-//         `;
-//     phoneContainer.appendChild(phoneDiv);
-//   });
-// };
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phoneContainer.innerHTML = "";
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.className = "single-result row align-items-center my-3 p-3";
+    phoneDiv.innerHTML = `
+      <div class="phone-details">
+      <div class="profile-pic w-50 m-auto rounded">
+          <img class="w-50" src="" alt="">
+      </div>
+      <h1>Phone Name: cool</h1>
+      <h3>Brand:oh that working</h3>
+      <h4>Slug: do</h4>
+  </div>
+  `;
+    phoneContainer.appendChild(phoneDiv);
+  });
+};
