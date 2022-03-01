dc59292000f191f16a0fb0be1bfc60b85c6abdc6
Alneed Roy <alneedrs2@gmail.com>
2022-03-01 17:25:17 +0600

    bonus part done

--- js/app.js
+++ js/app.js
@@ -1,74 +1,89 @@
+// search mobile option
+
 const searchPhone = () => {
-  const searchText = document.getElementById("search-field").value;
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  // load data
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayPhones(data.data));
-};
-// about phone
-const displayPhones = (phones) => {
-  const phoneContainer = document.getElementById("phone-container");
-  phoneContainer.innerHTML = "";
-  if (!phones) {
-    const displayError = (error) => {
-      const errorTag = document.getElementById("error-message");
-      errorTag.innerText = error;
-    };
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  if (searchText == "") {
+    alert(" Please Enter a Phone Name!");
+  } else {
+    searchField.value = "";
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displaySearchResult(data.data));
   }
-  phones.forEach((phone) => {
-    const phoneDiv = document.createElement("div");
-    phoneDiv.className = "card col-lg-4 my-1 p-3";
-    phoneDiv.innerHTML = `
-      <div class="phone-details">
-      <div class="profile-pic w-50 m-auto rounded">
-          <img class="w-50" src="${phone.image}" alt="">
-      </div>
-      <h3>Phone Name: ${phone.phone_name}</h3>
-      <h4>Brand: ${phone.brand} </h4>
-  </div>
-  <div class="mt-4">
-        <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-success">Phone Details</button>
-  </div>
-  `;
-    phoneContainer.appendChild(phoneDiv);
-  });
 };
-// about phone details
+
+// search result
+
+const displaySearchResult = (data) => {
+  const searchResult = document.getElementById("search-result");
+  searchResult.textContent = "";
+
+  console.log(data);
+  if (data.length == 0) {
+    alert("Result Not Found! Please Enter a Valid Phone Name.");
+  } else {
+    data.slice(0, 20).forEach((datas) => {
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+        
+        <div   class="card align-items-center">
+        
+        
+        <img class="w-50 p-5" src="${datas.image}  " class="card-img-top" alt="...">
+        <div class="card-body">
+        <h4 class="card-title">${datas.phone_name}</h4>
+        <h5> Brand: ${datas.brand}</h5>  
+        </div>
+        <button onclick="(loadPhoneDetail('${datas.slug}'))" class="btn btn-primary">Phone Details</button>
+        `;
+      searchResult.appendChild(div);
+    });
+  }
+};
+
+// phone details
 
 const loadPhoneDetail = (id) => {
-  // console.log(id);
+  console.log(id);
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-  // console.log(url);
+  console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayloadPhoneDetail(data.data));
 };
 
-const displayloadPhoneDetail = (phone) => {
-  // console.log(phone);
+const displayloadPhoneDetail = (datas) => {
+  console.log(datas);
 
   phoneDetails = document.getElementById("phone-details");
   phoneDetails.innerHTML = "";
   const div = document.createElement("div");
   div.classList.add("col");
   div.innerHTML = `
-      <div class ="card-body ">
-      <img src="${phone.image}">
-      <h3>Name: ${phone.name}</h3>
-      <h5>Realease Date: ${phone.releaseDate}</h5>
-      <h4>Main Features</h4>
-      <p> ${phone?.mainFeatures?.sensors}</p>
-      <p> ${phone?.mainFeatures?.chipSet}</p>
-      <p> ${phone?.mainFeatures?.displaySize}</p>
-      <p> ${phone?.mainFeatures?.memory}</p>
-      <p> ${phone?.mainFeatures?.storage}</p>
-      <h4>Other features</h4>
-      <h6>Bluetooth:${phone?.others?.Bluetooth}</h6>
-      <h6>NFS: ${phone?.others?.NFC}</h6>
-      <h6>GPS: ${phone?.others?.GPS}</h6>
-      <h6>Radio: ${phone?.others?.Radio}</h6>
-      <h6>Wifi: ${phone?.others?.WLAN}</h6>
+  <div class ="card-body ">
+  <img src="${datas.image}">
+  <h2>Name: ${datas.name}</h2>
+  <h5>Release Date: ${datas.releaseDate}</h5>
+  <h4>Main Features</h4>
+  <p>Platform: ${datas.mainFeatures?.chipSet}</p>
+  <p>Display: ${datas.mainFeatures?.displaySize}</p>
+  <p>Memory: ${datas.mainFeatures?.memory}</p>
+  <p>Storage: ${datas.mainFeatures?.storage}</p>
+  <h4>Special Features</h4>
+  <p>Sensors: ${datas.mainFeatures?.sensors}</p>
+  <h4>Other Features</h4>
+  <p>Bluetooth:${datas.others?.Bluetooth}</p>
+  <p>NFS: ${datas.others?.NFC}</p>
+  <p>GPS: ${datas.others?.GPS}</p>
+  <p>Radio: ${datas.others?.Radio}</p>
+  <p>USB: ${datas.others?.USB}
+  <p>WLAN: ${datas.others?.WLAN}</p>   
+  </div>
   `;
   phoneDetails.appendChild(div);
 };
